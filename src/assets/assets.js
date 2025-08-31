b186793157d88307c2384ed7c0111cc2f5a3de9d
KABANGA David <50810707+tipizzo@users.noreply.github.com>
2025-08-31 18:49:33 +0200

    Adding the header section

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,4 +1,4 @@
-import logo from './mklogo.jpg'
+import logo from './mklogo1.png'
 import logo_dark from './logo_dark.svg'
 import cross_icon from './cross_icon.svg'
 import menu_icon from './menu_icon.svg'
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <div className='absolute top-0 left-0 w-full z-10'>
         <div className='container mx-auto flex justify-between items-center py-4 px-6 md: px-20 lg:px-32 bg-transparent'>
-            <img src={assets.logo} alt="" />
+            <img src={assets.logo} alt="" style={{width: "200px", height: "130px"}} />
             <ul className='hidden md:flex gap-7 text-white'>
                 <a href='#Header' className='cursor-pointer hover:text-gray-400'>Home</a>
                 <a href='#Header' className='cursor-pointer hover:text-gray-400'>About</a>
