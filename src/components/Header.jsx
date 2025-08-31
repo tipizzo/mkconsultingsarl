1bfe7ccf6a5e7c76dd9259ccd1d46cbd1f53891d
KABANGA David <50810707+tipizzo@users.noreply.github.com>
2025-08-31 20:42:51 +0200

    Adding project section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,10 +6,10 @@ const Header = () => {
     <div className='min-h-screen mb-4 bg-cover bg-center flex items-center w-full overflow-hidden' style={{backgroundImage: "url('/header_img4.jpg')"}} id='Header'>
         <Navbar />
         <div className='container text-center mx-auto py-4 px-6 md:px-20 lg:px-32 text-white'>
-            <h2 className='text-5xl sm:text-6xl md:text-[82px] inline-block max-w-3xl font-semibold pt-20'>Small enough to listen, BIG ENOUGH TO DELIVER.</h2>
+            <h2 className='text-5xl sm:text-6xl md:text-[82px] inline-block max-w-3xl font-semibold pt-20'>Innovation et savoir-faire au service de vos constructions.</h2>
             <div className='space-x-6 mt-16'>
-                <a href='#Projects' className='border border-white px-8 py-3 rounded-md'>Projects</a>
-                <a href='#Contact' className='bg-blue-500 px-8 py-3 rounded-md'>Contact Us</a>
+                <a href='#Projects' className='border border-white px-8 py-3 rounded-md'>Nos Projets</a>
+                <a href='#Contact' className='bg-blue-500 px-8 py-3 rounded-md'>Contactez-nous</a>
             </div>
         </div>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,10 +24,10 @@ const Navbar = () => {
         <div className='container mx-auto flex justify-between items-center py-4 px-4 md:px-8 lg:px-16 bg-transparent'>
             <img src={assets.logo} alt="" style={{width: "200px", height: "130px"}} />
             <ul className='hidden md:flex gap-7 text-white'>
-                <a href='#Header' className='cursor-pointer hover:text-gray-400'>Home</a>
-                <a href='#About' className='cursor-pointer hover:text-gray-400'>About</a>
-                <a href='#Projects' className='cursor-pointer hover:text-gray-400'>Projects</a>
-                <a href='#Testimonials' className='cursor-pointer hover:text-gray-400'>Testimonials</a>
+                <a href='#Header' className='cursor-pointer hover:text-gray-400'>Accueil</a>
+                <a href='#About' className='cursor-pointer hover:text-gray-400'>A Propos de nous</a>
+                <a href='#Projects' className='cursor-pointer hover:text-gray-400'>Projets</a>
+                <a href='#Testimonials' className='cursor-pointer hover:text-gray-400'>Temoignages</a>
             </ul>
             <img onClick={() => setShowMobileMenu(true)} src={assets.menu_icon} className='md:hidden w-7 cursor-pointer' />
         </div>
@@ -38,10 +38,10 @@ const Navbar = () => {
                 <img onClick={() => setShowMobileMenu(false)} src={assets.cross_icon} className='w-6' alt="" />
             </div>
             <ul className='flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium'>
-                <a onClick={() => setShowMobileMenu(false)} href='#Header' className='px-4 py-2 rounded-full inline-block'>Home</a>
-                <a onClick={() => setShowMobileMenu(false)} href='#About' className='px-4 py-2 rounded-full inline-block'>About</a>
-                <a onClick={() => setShowMobileMenu(false)} href='#Projects' className='px-4 py-2 rounded-full inline-block'>Projects</a>
-                <a onClick={() => setShowMobileMenu(false)} href='#Testimonials' className='px-4 py-2 rounded-full inline-block'>Testimonials</a>
+                <a onClick={() => setShowMobileMenu(false)} href='#Header' className='px-4 py-2 rounded-full inline-block'>Accueil</a>
+                <a onClick={() => setShowMobileMenu(false)} href='#About' className='px-4 py-2 rounded-full inline-block'>A Propos de nous</a>
+                <a onClick={() => setShowMobileMenu(false)} href='#Projects' className='px-4 py-2 rounded-full inline-block'>Projets</a>
+                <a onClick={() => setShowMobileMenu(false)} href='#Testimonials' className='px-4 py-2 rounded-full inline-block'>Temoignages</a>
             </ul>
         </div>
     </div>
